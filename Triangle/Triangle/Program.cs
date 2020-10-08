ceec292d926ca1e0908654e861ac84d867c72943
Sasha1-0-0-9 <34281985+Sasha1-0-0-9@users.noreply.github.com>
2020-10-08 10:23:27 +0300

    Add files via upload

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle abc = new Triangle();
+            Triangle.Output(abc.SideA, abc.SideB, abc.SideC);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Input new length for side A");
+            abc.SideA = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Input new length for side B");
+            abc.SideB = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Input new length for side C");
+            abc.SideC = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        protected int a;
+        protected int b;
+        protected int c;
+
+        public Triangle ()
+        {
+            Random rand = new Random();
+            while (a + b <= c || a + c <= b || b + c <= a)
+            {
+                a = rand.Next(1, 15);
+                b = rand.Next(1, 15);
+                c = rand.Next(1, 15);
+            }
+        }
+
+        public int SideA
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if(value + b <= c || value + c <= b || value >= b + c)
+                {
+                    Console.WriteLine("Не iснує");
+                }
+                else
+                {
+                    Console.WriteLine("Iснує");
+                    a = value;
+                }
+            }
+        }
+
+        public int SideB
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value + a <= c || value + c <= a || value >= a + c)
+                {
+                    Console.WriteLine("Не iснує");
+                }
+                else
+                {
+                    Console.WriteLine("Iснує");
+                    b = value;
+                }
+            }
+        }
+
+        public int SideC
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                if (value + b <= a || value + a <= b || value >= b + a)
+                {
+                    Console.WriteLine("Не iснує");
+                }
+                else
+                {
+                    Console.WriteLine("Iснує");
+                    c = value;
+                }
+            }
+        }
+        static int Perimeter(int a, int b, int c)
+        {
+                return a + b + c;
+
+        }
+
+        static double Square(int a, int b , int c)
+        {
+                double halfPer = ((double)a + (double)b + (double)c) / 2.0;
+                Console.WriteLine(halfPer);
+                return Math.Sqrt(halfPer * (halfPer - a) * (halfPer - b) * (halfPer - c));
+        }
+
+        public static void Output(int a, int b, int c)
+        {
+            Console.WriteLine("A = {0} cm   B = {1} cm   C= {2} cm", a, b, c);
+            Console.WriteLine(" Perimetr: {0}", Perimeter(a, b, c));
+            Console.WriteLine("Square: {0}", Square(a, b, c));
+            Console.WriteLine();
+        }
+    }
+}
